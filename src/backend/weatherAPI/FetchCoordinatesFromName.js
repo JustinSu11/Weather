ddddda112cdcfc6570445c91ac44c549557df808
Justin <justin.nguyen77@yahoo.com>
2025-05-27 14:25:23 -0500

    replaced the check for string with checking to see if input has coords properties to determine if it is a browser position object

--- src/backend/weatherAPI/FetchCoordinatesFromName.js
+++ src/backend/weatherAPI/FetchCoordinatesFromName.js
@@ -7,8 +7,8 @@ const apiUrlForReverseGeocoding = process.env.API_URL_FOR_REVERSE_GEOCODING
 
 //returns the an object with the city name and the weather info
 export default async function fetchCoordinatesFromName(userInput) {
-    //check if input is a string indicating that it is a city name otherwise the input are coordinates
-    if (typeof userInput === 'string') {
+    //check if input is the position object from the browser, if it is then fetch weather info using the latitude and longitude
+    if (!userInput.coords) {
         try {
             const response = await axios.get(apiUrl, { params: { q: userInput, limit: '5', appid: apiKey}})
             const cityName = response.data[0].name
