746a12eeadc5e779013857838845164787f3d33e
Justin <justin.nguyen77@yahoo.com>
2025-05-27 16:28:23 -0500

    Updated placeholders to the correct object properties

--- src/frontend/components/WeatherInfoContainer.js
+++ src/frontend/components/WeatherInfoContainer.js
@@ -6,9 +6,10 @@ export default function WeatherInfoContainer(props) {
         <div className = "weather-info-container">
             <div className = "weather-info-header">Weather Info</div>
             <div className = "weather-info-content">
-                <div className = "weather-info-item">{props.weatherInfo}</div>
-                <div className = "weather-info-item">{props.weatherInfo}</div>
-                <div className = "weather-info-item">{props.weatherInfo}</div>
+                <div className = "weather-info-item">{props.weatherInfo.humidity}%</div>
+                <div className = "weather-info-item">{props.weatherInfo.feels_like}°</div>
+                <div className = "weather-info-item">{props.weatherInfo.weather[0].wind_speed}mph</div>
+                <div className = "weather-info-item">{props.weatherInfo.weather[0].wind_gust}mph</div>
             </div>
         </div>
     )
