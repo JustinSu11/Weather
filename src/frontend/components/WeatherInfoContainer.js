db17caf33545b51cd2fa8803df07718a0a09a7fa
Justin <justin.nguyen77@yahoo.com>
2025-05-28 11:55:03 -0500

    Passed feels_like property to Math.round to removed decimal

--- src/frontend/components/WeatherInfoContainer.js
+++ src/frontend/components/WeatherInfoContainer.js
@@ -7,7 +7,7 @@ export default function WeatherInfoContainer(props) {
             <div className = "weather-info-header">Weather Info</div>
             <div className = "weather-info-content">
                 <div className = "weather-info-item">{props.weatherInfo.current.humidity}%</div>
-                <div className = "weather-info-item">{props.weatherInfo.current.feels_like}°</div>
+                <div className = "weather-info-item">{Math.round(props.weatherInfo.current.feels_like)}°</div>
                 <div className = "weather-info-item">{props.weatherInfo.current.wind_speed}mph</div>
             </div>
         </div>
