a20e9ab78652d457db45324f83109216391d57e4
Justin <justin.nguyen77@yahoo.com>
2025-05-29 09:52:41 -0500

    applied css to component

--- src/frontend/components/WeatherInfoContainer.js
+++ src/frontend/components/WeatherInfoContainer.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+// import css
+import './WeatherInfoContainer.css'
+
 export default function WeatherInfoContainer(props) {
     return (
         //container for selected city weather info
