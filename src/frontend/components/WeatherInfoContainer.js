b5e997dc304653d4e3da6a1422503139e98ecc97
Justin <justin.nguyen77@yahoo.com>
2025-05-30 13:22:20 -0500

    added borders to individual weather info items, split weather info items into two divs, title and content

--- src/frontend/components/WeatherInfoContainer.js
+++ src/frontend/components/WeatherInfoContainer.js
@@ -9,9 +9,30 @@ export default function WeatherInfoContainer(props) {
         <div className = "weather-info-container">
             <div className = "weather-info-header">Weather Info</div>
             <div className = "weather-info-content">
-                <div className = "weather-info-item">{props.weatherInfo.current.humidity}%</div>
-                <div className = "weather-info-item">{Math.round(props.weatherInfo.current.feels_like)}°</div>
-                <div className = "weather-info-item">{props.weatherInfo.current.wind_speed}mph</div>
+                <div className = "weather-info-item">
+                    <div className="weather-info-item-title">
+                        <p>Humidity</p>
+                    </div>
+                    <div className="weather-info-item-content">
+                        {props.weatherInfo.current.humidity}%
+                    </div>
+                </div>
+                <div className = "weather-info-item">
+                    <div className="weather-info-item-title">
+                        <p>Feels Like</p>
+                    </div>
+                    <div className="weather-info-item-content">
+                        {Math.round(props.weatherInfo.current.feels_like)}°
+                    </div>
+                </div>
+                <div className = "weather-info-item">
+                    <div className="weather-info-item-title">
+                        <p>Wind Speed</p>
+                    </div>
+                    <div className="weather-info-item-content">
+                        {props.weatherInfo.current.wind_speed}mph
+                    </div>
+                </div>
             </div>
         </div>
     )
