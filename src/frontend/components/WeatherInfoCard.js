654ef779f1c37707ff60ea301e852470f6d7e7be
Justin <justin.nguyen77@yahoo.com>
2025-06-02 14:12:29 -0500

    divider not showing between the list and city/temp so removing orientation to see if that fixes

--- src/frontend/components/WeatherInfoCard.js
+++ src/frontend/components/WeatherInfoCard.js
@@ -19,7 +19,7 @@ export default function WeatherInfoCard(prop) {
                         {Math.round(prop.selectedCity.weatherInfo.current.temp)}°
                     </Typography>
                 </div>
-                <Divider orientation='vertical'/>
+                <Divider />
                 <List>
                     <ListItem>
                         <ListItemText primary='Feels Like: ' secondary={`${prop.selectedCity.weatherInfo.current.feels_like}°`} />
