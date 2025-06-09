5768e56b9c1f3beaad85d1d76bb845ba4e0f5ee5
Justin <justin.nguyen77@yahoo.com>
2025-06-09 14:10:26 -0500

    Trying to remove conditional setting of options prop

--- src/frontend/components/SearchBar.js
+++ src/frontend/components/SearchBar.js
@@ -22,7 +22,7 @@ export default function SearchBar() {
                     setSuggestions(await fetchCitySuggestions(newInputValue))
                 }, 500)
             }}
-            options={suggestions.length > 0 ? suggestions.map((suggestion) => suggestion.name) : ['City not found']}
+            options={suggestions.map((suggestion) => suggestion.name)}
         />
     )
 }
